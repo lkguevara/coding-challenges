ce898362050e0fc233daa5ee5c39c8b51dbe50c1
Lian Katherin Guevara <liann.guevara@gmail.com>
2022-03-29 20:27:25 -0500

    name validation

--- 5-regex.js
+++ 5-regex.js
@@ -1,4 +1,4 @@
-// 1) Programa una función que dada una cadena de texto cuente el número de vocales y consonantes, pe. miFuncion("Hola Mundo") devuelva Vocales: 4, Consonantes: 5.
+// 1. Programa una función que dada una cadena de texto cuente el número de vocales y consonantes, pe. miFuncion("Hola Mundo") devuelva Vocales: 4, Consonantes: 5.
 
 const contarLetras = (cadena ="") => {
     // validar que el texto ingresado sea válido
@@ -25,3 +25,17 @@ const contarLetras = (cadena ="") => {
 
 contarLetras("Hola")
 
+// 2. Programa una función que valide que un texto sea un nombre válido, pe. miFuncion("Lian Guevara") devolverá verdadero.
+
+    const validarNombre = (nombre="") => {
+        // validar que el texto ingresado sea válido
+        if(!nombre) return console.log("no ingresaste una nombre");
+        if(typeof nombre !== "string") return console.log(`El valor ${nombre} ingresado no es una cadena de texto`);
+
+        let expReg = /^[A-Za-zÑñÁáÉéÍíÓóÚú\s]+$/g.test(nombre); //^ --> No puede haber nada antes de la expresión... $ --> no puede haber nada despues de la expresión .. \s espacios en blanco
+        return (expReg)
+            ?console.log(`"${nombre}", es un nombre válido`)
+            :console.log(`"${nombre}", es un nombre invalido`)
+    }
+
+    validarNombre("Lian");
