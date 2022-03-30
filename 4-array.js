a8ebf84bc59510ca38cf05bda10acc422a07db0c
Lian Katherin Guevara <liann.guevara@gmail.com>
2022-03-30 18:10:09 -0500

    Sorting an Array

--- 4-array.js
+++ 4-array.js
@@ -99,12 +99,16 @@ console.log(deletePattern("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz")); // imprime 1
     arrParImpares([1,2,3,4,5,6,7,8,9,0])
     
 
+// 11. Programa una función que dado un arreglo de números devuelva un objeto con dos arreglos, el primero tendrá los numeros ordenados en forma ascendente y el segundo de forma descendiente, pe. miFuncion([7, 5,7,8,6]) devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }.
+    
+    const arrAscenDescent = (arreglo) => {
+        return console.log(`Arreglo ordenado ascendente: ${arreglo.sort(function(a, b){return a - b})} y  descendiente: ${arreglo.sort(function(a, b){return b - a})}`)
+    }
+    arrAscenDescent([7,5,7,8,6])
+
+
+// 12. Programa una función que dado un arreglo de elementos, elimine los duplicados, pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true]) devolverá ["x", 10, 2, "10", true].
 
 
 
-
-
-
-// 24) Programa una función que dado un arreglo de números devuelva un objeto con dos arreglos, el primero tendrá los numeros ordenados en forma ascendente y el segundo de forma descendiente, pe. miFuncion([7, 5,7,8,6]) devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }.
-// 25) Programa una función que dado un arreglo de elementos, elimine los duplicados, pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true]) devolverá ["x", 10, 2, "10", true].
-// 26) Programa una función que dado un arreglo de números obtenga el promedio, pe. promedio([9,8,7,6,5,4,3,2,1,0]) devolverá 4.5.
+// 13. Programa una función que dado un arreglo de números obtenga el promedio, pe. promedio([9,8,7,6,5,4,3,2,1,0]) devolverá 4.5.
