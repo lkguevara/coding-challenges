c375dc9bedfe7b0ec8922112fbf45e849f3bdbec
Lian Katherin Guevara <liann.guevara@gmail.com>
2022-03-29 20:39:27 -0500

    email validation

--- 5-regex.js
+++ 5-regex.js
@@ -39,3 +39,27 @@ contarLetras("Hola")
     }
 
     validarNombre("Lian");
+
+// 3. Programa una función que valide que un texto sea un email válido, pe. miFuncion("jonmircha@gmail.com") devolverá verdadero
+
+    /* email correcto: 
+        - Antes del @ deberá ir: *letras *números *guiones *puntos.
+        - Después del @ y antes del .: *letras *números *guiones medios
+        - Después del .: deberá validar que al menos existan 2 caracteres
+    */
+
+    const validarEmail = (email="") => {
+        // validar que el texto ingresado sea válido
+        if(!email) return console.log("no ingresaste una email");
+        if(typeof email !== "string") return console.log(`El valor ${email} ingresado no es una cadena de texto`);
+
+        let expReg = /[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})/i.test(email); 
+        return (expReg)
+            ?console.log(`"${email}", es un email válido`)
+            :console.log(`"${email}", es un email invalido`)
+    }
+
+    validarEmail(); //no ingresaste una email
+    validarEmail(34); //El valor 34 ingresado no es una cadena de texto
+    validarEmail("lian,kat@gmail"); //"lian,kat@gmail", es un email invalido
+    validarEmail("liann.guevara@gmail.com"); //"liann.guevara@gmail.com", es un email válido
