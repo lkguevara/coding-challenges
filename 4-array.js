dea5fd7518bf652d804029be80e6554a152e059a
Lian Katherin Guevara <liann.guevara@gmail.com>
2022-04-14 19:19:24 -0500

    reto 25 días - día 2

--- 4-array.js
+++ 4-array.js
@@ -149,20 +149,5 @@ console.log(deletePattern("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz")); // imprime 1
 
     promedioArreglo([9,8,7,6,5,4,3,2,1,0]) 
 
-// 13. Considera una lista/array de ovejas. Cada oveja tiene un nombre y un color. Haz una función que devuelva una lista con todas las ovejas que sean de color rojo y que además su nombre contenga tanto las letras n Y a, sin importar el orden, las mayúsculas o espacios.
-
-    function contarOvejas(ovejas) {
-        
-        return console.log(ovejas.filter((oveja) => oveja.color === 'rojo' && oveja.name.indexOf("Na") !== -1)   ) 
-        
-    }
-
-    contarOvejas([
-        { name: 'Noa', color: 'azul' },
-        { name: 'Euge', color: 'rojo' },
-        { name: 'Navidad', color: 'rojo' },
-        { name: 'Ki Na Ma', color: 'rojo'},
-        { name: 'AAAAAaaaaa', color: 'rojo' },
-        { name: 'Nnnnnnnn', color: 'rojo'}])
 
    
