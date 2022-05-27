755f5ec6390fe8bd7290dbdb0e5af751d1f1ae4f
Lian Katherin Guevara <liann.guevara@gmail.com>
2022-05-27 18:37:13 -0500

    Ejercicio DOM - 01 Menú hamburguesa

--- DOM/01-hamburguerMenu.js
+++ DOM/01-hamburguerMenu.js
@@ -1,13 +1,17 @@
 const btnHamburguer = document.querySelector(".panel-btn");
 const panel = document.querySelector(".panel")
+const panelbtn = document.querySelector(".is-active")
 
 btnHamburguer.addEventListener("click", (e) => {
     // alert("Hola")
     if (panel.classList.contains("active")) {
         panel.classList.remove("active");
-
+        panelbtn.classList.remove("is-active")
     }else{
         panel.classList.add("active")
+        panelbtn.classList.add("is-active");
     }
 })
+
+
 
