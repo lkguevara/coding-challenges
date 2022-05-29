2f484ba33ee1dab610993f288f9cb6983e3330e4
Lian Katherin Guevara <liann.guevara@gmail.com>
2022-05-29 15:29:13 -0500

    Corrección menú hamburguesa

--- DOM/01-hamburguerMenu.js
+++ DOM/01-hamburguerMenu.js
@@ -2,15 +2,25 @@ const btnHamburguer = document.querySelector(".panel-btn");
 const panel = document.querySelector(".panel")
 const panelbtn = document.querySelector(".is-active")
 
+/*
 btnHamburguer.addEventListener("click", (e) => {
-    // alert("Hola")
-    if (panel.classList.contains("active")) {
-        panel.classList.remove("active");
-        panelbtn.classList.remove("is-active")
+  
+    if (panel.classList.contains("is-active")) {
+        panel.classList.remove("is-active");
+  
     }else{
-        panel.classList.add("active")
-        panelbtn.classList.add("is-active");
+        panel.classList.add("is-active")
+
     }
+})
+
+*/
+
+// otra forma, aplicando la X
+btnHamburguer.addEventListener("click", (e) => {
+  
+    document.querySelector(".panel").classList.toggle("is-active");
+    document.querySelector(".panel-btn").classList.toggle("is-active");
 })
 
 
