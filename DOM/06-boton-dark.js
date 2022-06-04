ba2dd944386042748ae488f062b002f987b85b55
Lian Katherin Guevara <liann.guevara@gmail.com>
2022-06-04 18:19:29 -0500

    Ejercicio DOM - aplicando local Storage al modo dark

--- DOM/06-boton-dark.js
+++ DOM/06-boton-dark.js
@@ -4,17 +4,42 @@ let selectores = document.querySelectorAll("[data-dark]");
 
 let moon = "🌙", sun = "☀️"
 
+const lightMode = () => {
+    selectores.forEach(e => e.classList.remove('dark-mode'));
+    btn.textContent = moon;
+    localStorage.setItem('theme', "light");
+}
+
+const darkMode = () => {
+    selectores.forEach(e => e.classList.add('dark-mode'));
+    btn.textContent = sun;
+    localStorage.setItem('theme', "dark");
+}
 
 btn.addEventListener("click", e => {
     // console.log(btn.textContent);
 
     if (btn.textContent === moon) {
-        selectores.forEach(e => e.classList.add('dark-mode'));
-        btn.textContent = sun;
+        darkMode()
     }else {
-        selectores.forEach(e => e.classList.remove('dark-mode'));
-        btn.textContent = moon;
+        lightMode();
     }
 });
 
+// Aplicando local Storage
 
+document.addEventListener("DOMContentLoaded", e => {
+   if (localStorage.getItem('theme') === null ) {
+    localStorage.setItem('theme', 'light')
+   } 
+
+   if (localStorage.getItem('theme') === "light" ) {
+    lightMode()
+   } 
+
+   if (localStorage.getItem('theme') === "dark" ) {
+    darkMode()
+   } 
+
+
+})
