cc5e65bf554aaa1cc4c15dfcd7ac57fb6a2da594
lkguevara <liann.guevara@gmail.com>
2023-11-15 17:47:56 -0500

    ejercicio 09 - jsHero platzi

---
+++ js-hero-platzi/01-Fundamentos-arrays/09.calcTotal.js
@@ -0,0 +1,59 @@
+/**
+    Tienes un array de objetos que representan órdenes de compra con los siguientes atributos:
+        - customerName: string
+        - total: number
+        - delivered: boolean
+    
+    Tu reto es retornar la suma total de todas las órdenes de compra, para solucionarlo vas a encontrar una función llamada calcTotal que recibe un parámetro de entrada:
+        - orders: Un array con las órdenes de compra
+        - Dentro del cuerpo de la función calcTotal debes escribir tu solución.
+
+    Ejemplo:
+
+    Input:
+    [
+        {
+            customerName: "Nicolas",
+            total: 100,
+            delivered: true,
+        },
+        {
+            customerName: "Zulema",
+            total: 120,
+            delivered: false,
+        },
+        {
+            customerName: "Santiago",
+            total: 20,
+            delivered: false,
+        }
+    ]
+
+    Ouput:
+    240
+ */
+
+const calcTotal = (orders) =>{
+    const initialValue = 0;
+    const sumaTotal = orders.reduce((a,b) => a+b.total, initialValue)
+
+    return sumaTotal
+}
+
+console.log(calcTotal([
+    {
+        customerName: "Nicolas",
+        total: 100,
+        delivered: true,
+    },
+    {
+        customerName: "Zulema",
+        total: 120,
+        delivered: false,
+    },
+    {
+        customerName: "Santiago",
+        total: 20,
+        delivered: false,
+    }
+]))
